b140ea397368b8fe424d079fe6c0db998d8d9cf6
gusvs <bigvital@mail.ru>
2023-09-27 10:36:26 +0500

    Add Navbar, AppLink shared component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,17 @@
-import {Link} from 'react-router-dom'
 import './styles/index.scss'
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
 const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>Theme</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+
             <AppRouter />
         </div>
     );
