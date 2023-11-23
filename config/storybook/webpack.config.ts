da0944ccceddb781eeb732348e8caa51d3180f14
gusvs <bigvital@mail.ru>
2023-11-23 14:42:10 +0500

    API instance, ApiUrl, add profile, fetchProfileData, enable strict mode, fix typings, fix tests

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,15 +10,16 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.modules = [
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.modules = [
         path.resolve(__dirname, '../../src'),
         'node_modules',
     ];
-    config.resolve.extensions.push('.ts', '.tsx');
+    config!.resolve!.extensions!.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+    // @ts-ignore
+    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
         if (/svg/.test(rule.test as string)) {
             return { ...rule, exclude: /\.svg$/i };
         }
@@ -26,14 +27,15 @@ export default ({ config }: {config: webpack.Configuration}) => {
         return rule;
     });
 
-    config.module.rules.push({
+    config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module.rules.push(buildCssLoader(true));
+    config!.module!.rules.push(buildCssLoader(true));
 
-    config.plugins.push(new DefinePlugin({
-        __IS_DEV__: true,
+    config!.plugins!.push(new DefinePlugin({
+        __IS_DEV__: JSON.stringify(true),
+        __API__: JSON.stringify(''),
     }));
 
     return config;
